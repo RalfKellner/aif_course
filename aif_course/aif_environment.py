3f50c596299598f6ea9d91be39cde664761dfc88
RalfKellner <ralf.kellner@uni-passau.de>
2022-07-14 11:00:47 +0200

    add new versions of environment

--- aif_course/aif_environment.py
+++ aif_course/aif_environment.py
@@ -341,6 +341,182 @@ class TradingEnvironment(gym.Env):
         return self.data_source.take_step()[0]
 
 
+############################################################################################################################################
+
+# a class for downloading and processing of stock data
+class DataLoaderNew():
+    def __init__(self, ticker, start_date, end_date, use_variables = [], scaler = None, trading_days = 252):
+
+        '''
+        ticker (string): ticker for the company
+        start_date (string): start date for company data, format YYYY-MM-DD
+        end_date (string): start date for company data, format YYYY-MM-DD
+        use_variables (list of strings): a list of technical indicators to be used as state variables, e.g., ['AD', 'ABER_ZG_5_15']
+        scaler: a scaler from the training data, only meant to be imported for test data
+        trading_days (int): the number of trading days per episode
+        open_close_return (boolean): set to true if returns are supposed to be calculated from the beginning of the next trading day until its end
+        '''
+
+        self.ticker = ticker
+        self.start_date = start_date
+        self.end_date = end_date
+        self.use_variables = use_variables
+        self.trading_days = trading_days
+        
+        self.load_data()
+        self.preprocess_data()
+
+        if scaler:
+            self.scaler = scaler
+        else:
+            self.scaler = StandardScaler()
+            self.scaler.fit(self.df.drop(['returns'], axis = 1).values)
+
+        
+        self.rnd_seeds = []
+
+    def load_data(self):
+
+        '''
+        This function uses the yfinance data to download OHLC Volume data
+
+        ticker, string: the abbreviation of companies at stock exchanges, e.g., AAPL
+        start_date, string: the beginning of the time series, format is YYYY-MM-DD
+        end_date, string: the end of the time series, format is YYYY-MM-DD
+        '''
+
+        # define the yfinance ticker instance
+        tmp_ticker = yf.Ticker(self.ticker)
+        # retrieve data
+        tmp_data = tmp_ticker.history(start = self.start_date, end = self.end_date)
+        # reduce data to OHLC-Volume data
+        self.df = tmp_data[['Open', 'High', 'Low', 'Close', 'Volume']]
+        self.raw_data = self.df.copy()
+
+    def preprocess_data(self):
+
+        '''
+        This function determines daily returns and technical indicators. It is possible to use a subset of
+        technical indicators by providing a list with names of these indicators. In the default setting
+        indicators with a frequency of NAs which is higher than 5% are dropped
+
+        use_variables, list of strings: names of technical indicators to use, e.g., ['AD', 'ABER_ZG_5_15']
+        if None, all indicators are used
+        '''
+        # calculate returns
+        self.df.loc[:, 'returns'] = np.log(self.df.Close) - np.log(self.df.Close.shift(1))        
+
+        # index of the return column to exclude raw data columns later
+        idx = self.df.shape[1]
+
+        # get all technical indicators first
+        self.df.ta.strategy()
+        # delete all tis with potential leakage
+        self.df.drop(['DPO_20', 'ISA_9', 'ISB_26', 'ITS_9', 'ICS_26', 'IKS_26'], axis = 1, inplace = True)
+
+        # default case, determine all indicators and delete columns with too many missing values
+        if len(self.use_variables) == 0:
+            self.df = self.df.iloc[:, (idx - 1):]
+            cols_to_drop = self.df.columns[self.df.isnull().mean() > 0.05]
+            self.df.drop(cols_to_drop, axis = 1, inplace = True)
+        # otherwise use variables as desired    
+        else:
+            self.df = self.df[['returns'] + self.use_variables]
+
+        # monitor how many observations (rows) are lost by dropping rows with missing values
+        n_obs_before = self.df.shape[0]
+        self.df.dropna(inplace = True)
+        n_obs_after = self.df.shape[0]
+        dropped_obs = n_obs_before - n_obs_after
+
+        print(f'{dropped_obs} observations have been deleted due to missing data')
+        print(f'This equals {(dropped_obs / n_obs_before)*100:.2f}% of the data')
+
+        # this ensures to have a decent amount of observations to train the agent
+        assert len(self.df) > 2*self.trading_days, f'Data needs to inlude at least {2*self.trading_days} observations! Choose a longer time period...'
+        print(f'Time period used for training or testing starts at:{self.df.index[self.trading_days]} and ends at: {self.df.index[-1]}')
+        self.min_values = self.df.drop(['returns'], axis = 1).min()
+        self.max_values = self.df.drop(['returns'], axis = 1).max()
+    
+    def reset(self, seed = None):
+        
+        '''
+        This function sets the step back to zero and randomly starts in the timeline of data.
+        A seed can be given to reproduce results or if is not given is recorded at each reset
+        '''
+        high = len(self.df.index) - self.trading_days
+        
+        if seed:
+            np.random.seed(seed)
+
+        self.offset = np.random.randint(low = self.trading_days, high = high)
+        self.step = 0
+        
+    def take_step(self):        
+        """Returns data for current trading day and done signal"""
+        state = self.df.drop(['returns'], axis = 1).iloc[self.offset + self.step].values.reshape(1, -1)
+        state_scaled = self.scaler.transform(state).flatten()
+        obs = np.concatenate((self.df[['returns']].iloc[self.offset + self.step].values, state_scaled), axis = 0)
+        self.step += 1
+        done = self.step > self.trading_days
+        return obs, done
+    
+    # some utility functions, not for direct use
+    def get_scaled_df_full_(self):
+        states_scaled = self.scaler.transform(self.df.drop(['returns'], axis = 1).values)
+        obs_full = np.concatenate((self.df[['returns']].values, states_scaled), axis = 1)
+        return obs_full
+    
+    def financial_plot_(self):
+        '''
+        Function to visualize time series data, taken from here:
+        https://towardsdatascience.com/basics-of-ohlc-charts-with-pythons-matplotlib-56d0e745a5be
+        '''
+        x = np.arange(0,len(self.raw_data))
+        fig, (ax, ax2) = plt.subplots(2, figsize=(12,8), gridspec_kw={'height_ratios': [4, 1]})
+
+        for idx, (dt, val) in enumerate(self.raw_data.iterrows()):
+            color = '#2CA453'
+            if val['Open'] > val['Close']: color= '#F04730'
+            ax.plot([x[idx], x[idx]], [val['Low'], val['High']], color=color)
+            ax.plot([x[idx], x[idx]-0.1], [val['Open'], val['Open']], color=color)
+            ax.plot([x[idx], x[idx]+0.1], [val['Close'], val['Close']], color=color)
+
+        # ticks top plot
+        date_step = int(self.raw_data.shape[0] / 5)
+        dates = [d.date() for d in self.raw_data.index]
+        ax2.set_xticks(x[::date_step])
+        ax2.set_xticklabels(dates[::date_step])
+        ax.set_xticks(x, minor=True)# labels
+        ax.set_ylabel('')
+        ax2.set_ylabel('Volume')# grid
+        ax.xaxis.grid(color='black', linestyle='dashed', which='both', alpha=0.1)
+        ax2.set_axisbelow(True)
+        ax2.yaxis.grid(color='black', linestyle='dashed', which='both', alpha=0.1)# remove spines
+        ax.spines['right'].set_visible(False)
+        ax.spines['left'].set_visible(False)
+        ax.spines['top'].set_visible(False)
+        ax2.spines['right'].set_visible(False)
+        ax2.spines['left'].set_visible(False)# plot volume
+        ax2.bar(x, self.raw_data['Volume'], color='lightgrey')
+        # get max volume + 10%
+        mx = self.raw_data['Volume'].max()*1.1
+        # define tick locations - 0 to max in 4 steps
+        yticks_ax2 = np.arange(0, mx+1, mx/4)
+        # create labels for ticks. Replace 1.000.000 by 'mi'
+        yticks_labels_ax2 = ['{:.2f} mi'.format(i/1000000) for i in yticks_ax2]
+        ax2.yaxis.tick_right() # Move ticks to the left side
+        # plot y ticks / skip first and last values (0 and max)
+        plt.yticks(yticks_ax2[1:-1], yticks_labels_ax2[1:-1])
+        plt.ylim(0,mx)
+
+        # title
+        ax.set_title(f'{self.ticker} stock price\n', loc='left', fontsize=20)
+        # no spacing between the subplots
+        plt.subplots_adjust(wspace=0, hspace=0)
+        plt.show()
+
+
 class TradingSimulatorNew:
     def __init__(self, trading_costs_bps):
         self.trading_costs_bps = trading_costs_bps
@@ -419,7 +595,7 @@ class TradingEnvironmentNew(gym.Env):
                  use_variables = [],
                  scaler = None):
         
-        self.data_source = DataLoader(ticker = ticker,
+        self.data_source = DataLoaderNew(ticker = ticker,
                                       start_date = start_date,
                                       end_date = end_date,
                                       use_variables = use_variables,
